da24d8c81c79b9eeab6559f4e00f528e21fbe88b
Joseph Gabriel <57839839+jhembe@users.noreply.github.com>
2023-05-10 11:04:55 +0300

    Update deadlock.sh
    Version 2. Fixed process listing mechanism

--- deadlock.sh
+++ deadlock.sh
@@ -8,28 +8,35 @@ elif [[ "$XDG_CURRENT_DESKTOP" == *"KDE"* ]]; then
   desktop_environment="kde"
 fi
 
-# Get a list of all processes and their open files
-processes=$(lsof)
+# Get a list of all processes and their states
+processes=$(ps -eo pid,state,command)
 
 # Check each process for any blocked or stuck state
 while read -r line; do
-  pid=$(echo "$line" | awk '{print $2}')
-  name=$(ps -p "$pid" -o comm=)
-  state=$(echo "$line" | awk '{print $8}')
+  # Extract the process ID, state, and command from the output
+  pid=$(echo "$line" | awk '{print $1}')
+  state=$(echo "$line" | awk '{print $2}')
+  command=$(echo "$line" | awk '{$1=""; $2=""; print $0}')
 
-  # If a process is stuck, print the PID, name, and state
-  if [[ "$state" == "BLOCKED" || "$state" == "STUCK" ]]; then
-    echo "Deadlock detected: process $pid ($name) is $state" >> /var/log/deadlock.log
+  # If a process is stuck, print the PID, command, and state
+  if [[ "$state" == "D" || "$state" == "R+" ]]; then
+    echo "Deadlock detected: process $pid ($command) is $state" >> /var/log/deadlock.log
 
     # Display a desktop notification using the appropriate command
     if [[ "$desktop_environment" == "gnome" ]]; then
-      notify-send "Deadlock detected" "Process $pid ($name) is $state"
+      notify-send "Deadlock detected" "Process $pid ($command) is $state"
     elif [[ "$desktop_environment" == "kde" ]]; then
-      kdialog --title "Deadlock detected" --passivepopup "Process $pid ($name) is $state" 5
+      kdialog --title "Deadlock detected" --passivepopup "Process $pid ($command) is $state" 5
     else
-      xdg-open "https://www.example.com/deadlock-detected?pid=$pid&name=$name&state=$state"
+      xdg-open "https://www.example.com/deadlock-detected?pid=$pid&command=$command&state=$state"
     fi
   fi
 done <<< "$processes"
+
+
+
+
+
+
 
 
